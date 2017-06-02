cba9427f982d807080050d2732f8b4b5952c6f42
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-02 11:27:41 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -3,7 +3,7 @@
 // @description Мой самый первый юзерскрипт 
 // @author Maxim Bezrukavnikov
 // @license GNU v3
-// @version 1.006
+// @version 1.007
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*
@@ -12,28 +12,14 @@
 // @include http://pikabu.ru/*
 // @include https://www.alienwarearena.com/*
 // ==/UserScript==
-// [1] Оборачиваем скрипт в замыкание, для кроссбраузерности (opera, ie)
-(function (window, undefined) {  // [2] нормализуем window
-    var w;
-    if (typeof unsafeWindow != undefined) {
-        w = unsafeWindow
-    } else {
-        w = window;
-    }
-    // В юзерскрипты можно вставлять практически любые javascript-библиотеки.
-    // Код библиотеки копируется прямо в юзерскрипт.
-    // При подключении библиотеки нужно передать w в качестве параметра окна window
-    // Пример: подключение jquery.min.js
-    // (function(a,b){function ci(a) ... a.jQuery=a.$=d})(w);
 
-    // [3] не запускаем скрипт во фреймах
-    // без этого условия скрипт будет запускаться несколько раз на странице с фреймами
-    if (w.self != w.top) {
-        return;
-    }
-    // [4] дополнительная проверка наряду с @include
-    if (/http:\/\/userscripts.org/.test(w.location.href)) {
-        //Ниже идёт непосредственно код скрипта
-        alert("Userscripts приветствует вас навязчивым окном.");
-    }
-})(window);
+var logo = document.createElement(«div»);
+logo.innerHTML = '
+<div style=«margin: 0pt auto; width: 800px; text-align: center;»>
+<h1 style=«margin: 15px;»>' +
+'Hello World habrahabr! ' +
+'</h1>
+</div>
+';
+
+document.body.insertBefore(logo, document.body.firstChild);

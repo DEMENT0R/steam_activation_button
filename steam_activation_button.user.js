bbcef30c5377345456ef89bf8655ba496e2756de
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-02 14:12:47 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -3,7 +3,7 @@
 // @description Мой самый первый юзерскрипт 
 // @author Maxim Bezrukavnikov
 // @license GNU v3
-// @version 1.011
+// @version 1.012
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*
@@ -17,7 +17,7 @@
     'use strict';
 
     var logo = document.createElement("div");
-logo.innerHTML = '<div style="border:none solid gray; width: 32px; height:32px; right:10px; border-radius:15px; position:fixed; top:3%; z-index:2000; background: #fff;">' +
+logo.innerHTML = '<div style="border:none solid gray; width: 32px; height:32px; right:10px; border-radius:5px; position:fixed; top:3%; z-index:2000; background: #fff;">' +
     '<a href="steam://open/activateproduct"><img src="https://steamuserimages-a.akamaihd.net/ugc/775029780384927193/6521F582D9EFF8E5BB747F8B940810F42DB597AF/" align="center" width="32px" height="32px" alt="Input Key" /></a>' + 
     '</div>';
 

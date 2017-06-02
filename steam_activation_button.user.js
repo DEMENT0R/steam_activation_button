1360d2b663a16f61e7eb6c8a6e7940399dcb2abc
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-02 13:55:38 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -3,7 +3,7 @@
 // @description Мой самый первый юзерскрипт 
 // @author Maxim Bezrukavnikov
 // @license GNU v3
-// @version 1.007
+// @version 1.009
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*
@@ -13,9 +13,16 @@
 // @include https://www.alienwarearena.com/*
 // ==/UserScript==
 
-var logo = document.createElement(«div»);
-logo.innerHTML = '<div style=«margin: 0pt auto; width: 800px; text-align: center;»><h1 style=«margin: 15px;»>' +
-'Hello World habrahabr! ' +
-'</h1></div>';
+(function() {
+    'use strict';
+
+    // Your code here...
+    var logo = document.createElement("div");
+//logo.innerHTML = '<div style=«margin: 0pt auto; width: 800px; text-align: center;»>' +
+logo.innerHTML = '<div style="border:2px solid red; width: 100px; height:56px; right:10px; border-radius:15px; position:fixed; top:34%; z-index:2000; background: #fff;">' +
+    '<a href="steam://open/activateproduct">input key</a>' + 
+    '</div>';
+//<style type="text/css">#right {border:2px solid red; width: 100px; height:56px; right:10px; border-radius:15px; position:fixed; top:34%; z-index:2000; background: #fff;}</style>
 
 document.body.insertBefore(logo, document.body.firstChild);
+})();

4c21815c425e174bb10ee7e97035bae085ab9345
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-02 14:05:13 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -3,7 +3,7 @@
 // @description Мой самый первый юзерскрипт 
 // @author Maxim Bezrukavnikov
 // @license GNU v3
-// @version 1.009
+// @version 1.010
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*
@@ -19,8 +19,8 @@
     // Your code here...
     var logo = document.createElement("div");
 //logo.innerHTML = '<div style=«margin: 0pt auto; width: 800px; text-align: center;»>' +
-logo.innerHTML = '<div style="border:2px solid red; width: 100px; height:56px; right:10px; border-radius:15px; position:fixed; top:34%; z-index:2000; background: #fff;">' +
-    '<a href="steam://open/activateproduct">input key</a>' + 
+logo.innerHTML = '<div style="border:none solid gray; width: 64px; height:64px; right:10px; border-radius:15px; position:fixed; top:3%; z-index:2000; background: #fff;">' +
+    '<a href="steam://open/activateproduct"><img src="https://steamuserimages-a.akamaihd.net/ugc/775029780384927193/6521F582D9EFF8E5BB747F8B940810F42DB597AF/" align="center" width="64px" height="64px" alt="Input Key" /></a>' + 
     '</div>';
 //<style type="text/css">#right {border:2px solid red; width: 100px; height:56px; right:10px; border-radius:15px; position:fixed; top:34%; z-index:2000; background: #fff;}</style>
 

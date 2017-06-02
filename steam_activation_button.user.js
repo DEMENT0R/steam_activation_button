a0a30e744fbd771fe9b601f814a351b8de44e38a
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-02 11:18:18 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -3,7 +3,7 @@
 // @description Мой самый первый юзерскрипт 
 // @author Maxim Bezrukavnikov
 // @license GNU v3
-// @version 1.005
+// @version 1.006
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*

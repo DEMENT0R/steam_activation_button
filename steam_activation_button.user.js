5a0f6e0e67e4f7e9b07bb9a09742417f9af7d600
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-04 00:16:55 +1000

    Update steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -6,6 +6,7 @@
 // @downloadURL https://github.com/DEMENT0R/steam_activation_button/raw/master/steam_activation_button.user.js
 // @updateURL https://github.com/DEMENT0R/steam_activation_button/raw/master/steam_activation_button.user.js
 // @resource KeyIcon https://github.com/DEMENT0R/steam_activation_button/raw/master/key.png
+// @resource myCustomCss https://github.com/DEMENT0R/steam_activation_button/raw/master/custom.css
 // @license GNU v3
 // @version 1.015
 // @include habrahabr.ru/*
@@ -33,5 +34,7 @@ logo.innerHTML = '<div style="border:none solid gray; width: 32px; height:32px; 
     '<a href="steam://open/activateproduct"><img src="https://github.com/DEMENT0R/steam_activation_button/raw/master/key.png" align="center" width="32px" height="32px" alt="Input Key" title="Input Key" /></a>' + 
     '</div>';
 
-document.body.insertBefore(logo, document.body.firstChild);
+    document.body.insertBefore(logo, document.body.firstChild);
+    
+    GM_addStyle (GM_getResourceText ("myCustomCss") );
 })();

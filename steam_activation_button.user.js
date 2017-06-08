bf52759a1ca1e8f38aa1f5e7a888890267cefb2e
Maxim Bezrukavnikov <dmntr@rambler.ru>
2017-06-08 11:33:49 +1000

    Create steam_activation_button.user.js

--- steam_activation_button.user.js
+++ steam_activation_button.user.js
@@ -9,7 +9,7 @@
 // @resource ListIcon https://github.com/DEMENT0R/steam_activation_button/raw/master/list.png
 // @resource myCustomCss https://github.com/DEMENT0R/steam_activation_button/raw/master/custom.css
 // @license GNU v3
-// @version 1.023
+// @version 1.024
 // @include habrahabr.ru/*
 // @include www.steamgifts.com/*
 // @include https://www.steamgifts.com/*
